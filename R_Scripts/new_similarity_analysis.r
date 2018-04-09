ba7614970f84e0f5c3d7613602cdfb14b4c6f7d0
Koech Nicholas <koechnicholas@gmail.com>
2018-04-09 22:49:49 +0300

    Returned list from "readAscii"

--- R_Scripts/new_similarity_analysis.r
+++ R_Scripts/new_similarity_analysis.r
@@ -18,3 +18,53 @@ installPackage <- function(pkg) {
   }
 }
 
+readAscii<- function(inFile) {
+  # Scan through the ASCII files while reading data
+  # Args:
+  #   inFile: Input ASCII file
+  # Return:
+  #   asciiData: A list of ascii header and grid data
+  
+  asciiData <- list()
+  for (i in 0:6) {
+    if (i != 6){
+      headerData <- scan(file = inFile, what = 'character', skip = i, nlines = 1, quiet=T)
+      asciiData[headerData[1]] <- as.numeric(headerData[2])
+    } else {
+      gridData <- scan(file = inFile, skip = i, quiet=T)
+      gridData[gridData == asciiData$NODATA_value] <- NA
+      dim(gridData) <- c(asciiData$NROWS * asciiData$NCOLS, 1)
+      asciiData$gridData <- gridData
+    }
+  }
+  return(asciiData)
+}
+
+similarityAnalysis <- function(numFiles, dirPath) {
+  # Performs similarity analysis
+  # Args:
+  #   numFiles: Number of files to be processed
+  #   fileDir: Path to where files are located
+  # Return: None
+  
+  installPackage(c("raster", "rgdal", "modEvA")) # Install/add packages
+  print('Starting similarity anlysi...')
+  
+  dataTable <- data.frame() # Create empty data frame
+  for (i in 1:numFiles){
+    readAscii(paste(dirPath, "tempAscii_", i, ".asc", sep = ""))
+  }
+  
+  
+  # for (cnt in 1:cntRaster)	{
+  #   inAscii = readAscii(paste(outFolder, "tempAscii_", cnt, ".asc", sep = ""))
+  #   if (cnt != 1) {
+  #     dataTable = cbind(dataTable,inAscii$gridData)
+  #   }
+  #   else	{
+  #     dataTable = inAscii$gridData
+  #   }
+  # }
+  
+  
+}
