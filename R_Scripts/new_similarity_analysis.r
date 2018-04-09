b4da4324a08dcfc9f5471db7112a5bd52408c15b
Koech Nicholas <koechnicholas@gmail.com>
2018-04-09 09:45:44 +0300

    Documented a function

--- R_Scripts/new_similarity_analysis.r
+++ R_Scripts/new_similarity_analysis.r
@@ -1,10 +1,20 @@
 installPackage <- function(pkg) {
+  # Install and add a package
+  # Args:
+  #   pkg: A vector of packages
+  # Return: None
+  
   for (i in 1:length(pkg)){
     if (!is.element(pkg[i], installed.packages()[,1])) {
       print(paste("Installing package:", pkg[i]))
-      ifelse(pkg[i] == "modEvA", install.packages("modEvA", repos="http://R-Forge.R-project.org"), install.packages(pkg[i], dep = TRUE))
+      if (pkg[i] == "modEvA"){
+        install.packages("modEvA", repos="http://R-Forge.R-project.org")
+      } else {
+        install.packages(pkg[i], dep = TRUE)
+      }
     }
     print(paste("Loading package:", pkg[i]))
     require(pkg[i], character.only = TRUE)	
   }
 }
+
